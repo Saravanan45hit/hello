177062fc2e0dc706cf45a033709c1e1d094d2cd5
Saravanan45hit <selvamsaravanan2003@gmail.com>
2022-05-22 09:38:02 +0530

    this this hello file

---
+++ class.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+class human{
+public:
+void display(){
+cout<<"asdf;lkjghqwerpoiutyzxcv.,mnnb";
+}
+};
+
+int main()
+{
+human beinnging;
+beinnging.display();
+return 0;
+}
